cfa8bda4abaa2ce86c8c821ef58fa70cca4b9775
zhiping <arya.zhangpika@gmail.com>
2025-10-15 20:38:04 +0630

    Fix bugs for deployment

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,15 +1,11 @@
 import Modules from "../Modules/page";
 import CourseStatus from "./Status";
 
-interface HomeProps {
-  params: Promise<{ cid: string }>;
-}
-
-export default async function Home({ params }: HomeProps) {
+export default function Home() {
   return (
     <div className="d-flex" id="wd-home">
       <div className="flex-fill me-5">
-        <Modules params={params} />
+        <Modules />
       </div>
       <div className="d-none d-lg-block">
         <CourseStatus />
