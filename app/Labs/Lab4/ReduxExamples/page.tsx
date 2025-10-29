7acde0d8ac3ee203f6cdb7e4e4fba6dc2c3de491
zhiping <arya.zhangpika@gmail.com>
2025-10-29 21:29:00 +0630

    Fix ReduxExamples SSR: add client-side mount check

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,12 +1,21 @@
 "use client";
+import { useEffect, useState } from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
 import TodoList from "./todos/TodoList";
 
-export const dynamic = 'force-dynamic';
-
 export default function ReduxExamples() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <div id="wd-redux-examples" className="mt-4">Loading...</div>;
+  }
+
   return(
     <div id="wd-redux-examples" className="mt-4">
       <h2>Redux Examples</h2>
