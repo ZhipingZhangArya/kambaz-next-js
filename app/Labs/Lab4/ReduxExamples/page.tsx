24d8bdbc054f1f38a9e5e82c7bc63c000f702492
zhiping <arya.zhangpika@gmail.com>
2025-11-12 21:10:50 +0630

    Fix ReduxExamples SSR error

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,10 +1,26 @@
 "use client";
+import { useState, useEffect } from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
 import TodoList from "./todos/TodoList";
 
 export default function ReduxExamples() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div id="wd-redux-examples" className="mt-4">
+        <h2>Redux Examples</h2>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return(
     <div id="wd-redux-examples" className="mt-4">
       <h2>Redux Examples</h2>
